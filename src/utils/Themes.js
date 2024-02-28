055af04cee50bba723d085d4799270f60d48477e
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-02-28 15:11:19 +0530

    a36

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,18 +1,18 @@
 export const darkTheme = {
-    bg: "	#20124d",
+    bg: "#1a1110",
     bgLight: "#1C1E27",
     primary: "#854CE6",
     text_primary: "#F2F3F4",
     text_secondary: "#b1b2b3",
-    card: "	#33007b",
-    card_light: '#8a6fdb',
+    card: "#343434",
+    card_light: '#100c08',
     button: "#e4fab2",
     white: "#FFFFFF",
     black: "#000000",
 }
 
 export const lightTheme = {
-    bg: "	#674ea7",
+    bg: "#674ea7",
     bgLight: "#f0f0f0",
     primary: "#be1adb",
     text_primary: "#111111",
