7c1fe9b38215aec329528ea40548ff5ff2fc69a9
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-04-20 01:16:34 +0530

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/KaranSingh52ED",
   resume:
-    "https://drive.google.com/file/d/15x2522wB3A9FfQDLMF1ua5Kh7NTm27QH/view?usp=sharing",
+    "https://drive.google.com/file/d/1Te2ZrvF3yqwlQJQKNCpZJMQWIhJeE0MT/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/karan-singh-875797260/",
   twitter: "",
   insta: "",
